79196695a0d7abc5a757c1584ace34c2d32c2476
Nabina Mallik <109237169+nabinamallik@users.noreply.github.com>
2023-12-31 14:49:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const socket = io("https://chat-gsm9.onrender.com");
+
+const form = document.querySelector("form");
+const m = document.getElementById("m");
+const messages = document.getElementById("messages");
+function scrollb(){
+  messages.scrollTop = messages.scrollHeight;
+}
+setInterval(scrollb, 300)
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  socket.emit("chat message", m.value);
+  m.value = "";
+});
+
+socket.on("chat message", function (msg) {
+  const li = document.createElement("li");
+  li.textContent = msg;
+  messages.appendChild(li);
+});
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle chat messages
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
